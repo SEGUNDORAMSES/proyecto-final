6f9d8398e6315898202ca59560336cfb2f17dcf5
SEGUNDORAMSES <cano1soto@gmail.com>
2024-08-06 11:42:17 -0600

    TAREAJS-5

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@
 //navbar
 import { mostrarhtml } from "/js/crearhtml.js"
 import { mostrar_datos_consola } from "/js/consultarJson.js"
+import { mostrar } from "/js/CREARHTML2.JS"
+import { crear_tabla } from "/js/CREARHTML2.JS";
 
 document.addEventListener('DOMContentLoaded', ev => {
     mostrarhtml()
     mostrar_datos_consola()
+    mostrar()
+    crear_tabla()
 })
 
