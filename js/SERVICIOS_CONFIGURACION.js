6a901487996dc38dbed7c60aac86cb13c7981569
SEGUNDORAMSES <cano1soto@gmail.com>
2024-08-06 20:12:46 -0600

    ft-entregable 6/8/2024

---
+++ js/SERVICIOS_CONFIGURACION.js
@@ -0,0 +1,25 @@
+export const obtener_datos_configuracion = async () => {
+let url="/js/Configuraciones.json"
+    try {
+
+        const response = await fetch(url);
+if (response.status=="200") {
+    
+}
+        const data = await response.json();
+
+        console.table(data);
+        
+}else{
+        console.log("error al consultar el api"+response.status);
+}
+     
+ } catch (error) {
+console.log("error en la red"+error)
+       
+
+    };
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 //alert("working")
 //navbar
-import { mostrarhtml } from "/js/crearhtml.js"
+import { mostrarhtml } from "/js/global"
 import { mostrar_datos_consola } from "/js/consultarJson.js"
 import { mostrar } from "/js/CREARHTML2.JS"
 import { crear_tabla } from "/js/CREARHTML2.JS";
-
+import { obtener_datos_configuracion } from "/js/SERVICIOS_CONFIGURACION.js";
 document.addEventListener('DOMContentLoaded', ev => {
     mostrarhtml()
     mostrar_datos_consola()
     mostrar()
     crear_tabla()
+    obtener_datos_configuracion()
 })
 
