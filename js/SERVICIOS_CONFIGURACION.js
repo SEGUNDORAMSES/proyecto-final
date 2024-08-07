c1c5ce01ae70ba6343438d5a5baeaa61210cc881
SEGUNDORAMSES <cano1soto@gmail.com>
2024-08-06 20:25:56 -0600

    ft-entregable 1 6/8/2024

--- js/SERVICIOS_CONFIGURACION.js
+++ js/SERVICIOS_CONFIGURACION.js
@@ -1,24 +1,24 @@
 export const obtener_datos_configuracion = async () => {
-let url="/js/Configuraciones.json"
+    let url = "/js/Configuraciones.json"
     try {
 
         const response = await fetch(url);
-if (response.status=="200") {
-    
-}
-        const data = await response.json();
+        if (response.status == "200") {
+            const data = await response.json();
 
-        console.table(data);
-        
-}else{
-        console.log("error al consultar el api"+response.status);
-}
-     
- } catch (error) {
-console.log("error en la red"+error)
-       
+            console.table(data);
+        }
+        else {
+            console.log("error al consultar el api" + response.status);
+        }
 
-    };
+    }
+
+ catch (error) {
+    console.log("error en la red" + error)
+}
+
+};
 
 
 
